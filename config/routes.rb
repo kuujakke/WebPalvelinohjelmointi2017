ea48eb5d5807a1993543748c0f7d5b7bd60c4f6e
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-01-17 21:27:56 +0200

    Root to breweries#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
-  root "breweries#show"
+  root "breweries_controller#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
