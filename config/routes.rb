ef64e36ed1ef2c4db2e0a22b984476d3de016dfd
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-01-27 19:27:42 +0200

    Viikon 2 tehtävä 12

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root 'breweries#index'
   resources :beers
   resources :breweries
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to: 'ratings#new'
-  post 'ratings', to: 'ratings#create'
+  resources :ratings, only: [:index, :new, :create, :destroy]
 end
