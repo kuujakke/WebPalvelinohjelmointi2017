4f7b979aaf37174ee350433b9871d9ad9e062216
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-02-14 21:37:11 +0200

    Viikko 5 - Tehtävä 2

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -10,4 +10,20 @@ describe "Places" do
     click_button "Search"
     expect(page).to have_content "Oljenkorsi"
   end
+  it "if API returns many, each is displayed" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+        [ Place.new( name:"Oljenkorsi1", id: 1 ), Place.new( name:"Oljenkorsi2", id: 2 ) ]
+    )
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+    expect(page).to have_content "Oljenkorsi1" and "Oljenkorsi2"
+  end
+  it "if nothing is returned by the API, a 'no locations in...' message is displayed" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return( [ ] )
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+    expect(page).to have_content "No locations in kumpula"
+  end
 end
