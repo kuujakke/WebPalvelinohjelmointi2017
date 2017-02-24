eac152589df6cdae511cdb637ad25989ded7c870
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-02-24 14:58:54 +0200

    Viikko 6 - Panimon statuksen muuttaminen

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users
   root 'breweries#index'
   resources :beers
-  resources :breweries
+  resources :breweries do
+    post 'toggle_activity', on: :member
+  end
   resources :ratings, only: [:index, :new, :create, :destroy]
   get 'signup', to: 'users#new'
   resource :session, only: [:new, :create, :destroy]
